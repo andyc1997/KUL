969894e3af86802fad68648c08df2be5d9687c2f
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2020-10-27 23:13:29 +0100

    Revise Q1 code in accordance to Toledo

--- question1_script.R
+++ question1_script.R
@@ -13,14 +13,16 @@ load(file.name)
 
 # Select and standardize data ---------------------------------------------
 f.std <- function(x) (x - mean(x))/sd(x)
-data_value <- wvs %>% select(starts_with('V_')) %>% 
-  apply(MARGIN = 2, FUN = f.std) %>% as.data.frame
+v_name <- wvs %>% select(starts_with('V_')) %>% colnames
+data_value <- wvs[, v_name] %>% apply(MARGIN = 2, FUN = f.std) %>% as.data.frame
 
 # Mean score by country ---------------------------------------------------------------------
 data_value <- data_value %>% aggregate(list(country = wvs$country), mean)
+data_value[, v_name] <- data_value[, v_name] %>% 
+  apply(MARGIN = 2, FUN = f.std)
 
 # PCA ---------------------------------------------------------------------
-fit_pca <- data_value %>% select(starts_with('V_')) %>% prcomp
+fit_pca <- data_value[, v_name] %>% prcomp
 
 # eigenvalues and cumulative proportion of variance explained
 eigenvalue <- fit_pca$sdev^2
@@ -33,6 +35,7 @@ plot(xtick, eigenvalue, type = 'l', col = 'blue', las = 1,
      ylab = 'Eigenvalues', xaxt = 'n')
 points(xtick, eigenvalue, pch = 19, col = 'black')
 axis(side = 1, at = xtick, labels = TRUE)
+abline(h = 1, col = 'green', lty = 2) # Kaiser's rule
 
 # Horn's procedure
 # Bootstrap from raw data
@@ -47,20 +50,18 @@ for (j in 1:10){
   eigenvalue_boot <- prcomp(boot_mat)$sdev ^ 2
   lines(xtick, eigenvalue_boot, type = 'l', col = 'red', lty = 2)
 }
-legend('topright', legend = c('Real Data', 'Bootstrapped Data'), 
-       col = c('blue', 'red'), lty = c(1, 2))
+legend('topright', legend = c('Real Data', 'Bootstrapped Data', 'Kaiser\'s Rule'), 
+       col = c('blue', 'red', 'green'), lty = c(1, 2, 2))
 
 # Loading matrix
-V <- data_value %>% select(starts_with('V_')) %>% apply(MARGIN = 2, FUN = var)
-V.inv_root <- (1 / sqrt(V)) %>% diag
 Z <- fit_pca$rotation
 D.root <- fit_pca$sdev %>% diag
 
-loading_mat <- V.inv_root %*% Z %*% D.root
+loading_mat <- Z %*% D.root
 loading_mat
 
 plot(loading_mat[, 1:2], xlab = 'PC 1', ylab = 'PC 2', main = 'Plot of Loading Matrix')
-pointLabel(loading_mat[, 1], loading_mat[, 2], colnames(data_value)[-1], col = 'red')
+pointLabel(loading_mat[, 1], loading_mat[, 2], v_name, col = 'red')
 
 # Biplot -----------------------------------------------------------------
 biplot(fit_pca, pc.biplot = TRUE, xlabs = data_value$country, col = c('blue', 'red'),
