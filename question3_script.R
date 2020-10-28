09ebb28862bfafb642c1c674dd2221770a340429
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2020-10-28 03:59:57 +0100

    Revised CFA model
    Only multi-group analysis left

--- question3_script.R
+++ question3_script.R
@@ -16,6 +16,7 @@ data_cfa <- wvs %>% filter(country %in% c('Netherlands', 'Malaysia')) %>%
   select(starts_with('J_')) %>% as.data.frame
 
 # Confirmatory Factor Analysis ----------------------------------------
+# Structure specified from EFA
 n <- nrow(data_cfa)
 cov_mat <- cov(data_cfa)
 cfa_relation <- 'fraudulent =~ NA*J_claiming_benefits + J_avoiding_fare +
@@ -27,13 +28,37 @@ cfa_relation <- 'fraudulent =~ NA*J_claiming_benefits + J_avoiding_fare +
                 marriage ~~ 1*marriage
                 violent ~~ 1*violent
                 fraudulent ~~ violent
-                marriage ~~ violent'
+                marriage ~~ violent
+                '
 
 fit_cfa <- cfa(cfa_relation, sample.cov = cov_mat, sample.nobs = n)
-summary(fit_cfa, fit.measures = TRUE)
-standardizedSolution(fit_cfa)
-modificationIndices(fit_cfa)
-#######################################
-# To be continued                     #
-#######################################
+fitmeasures(fit_cfa, c('cfi', 'tli', 'srmr', 'rmsea', 'chisq', 'df', 'pvalue'))
+summary(fit_cfa) # loadings
+standardizedSolution(fit_cfa) # Standardized solution & Reliability
+fit_cfa.mi <- modificationIndices(fit_cfa) # modification indices (mi)
+fit_cfa.mi[order(fit_cfa.mi$mi, decreasing = TRUE),] # rank relationship by decreasing mi
+
+# Update model structure by modification indices
+cfa_relation <- 'fraudulent =~ NA*J_claiming_benefits + J_avoiding_fare +
+                J_stealing_property + J_cheating_taxes + J_accept_bribe
+                marriage =~ NA*J_homosexuality + J_prostitution + J_abortion + 
+                J_divorce + J_sex_before_marriage + J_suicide
+                violent =~ NA*J_beat_wife + J_parents_beating_children + J_violence + J_suicide
+                fraudulent ~~ 1*fraudulent
+                marriage ~~ 1*marriage
+                violent ~~ 1*violent
+                fraudulent ~~ violent
+                marriage ~~ violent
+                J_homosexuality ~~ J_sex_before_marriage
+                '
+fit_cfa <- cfa(cfa_relation, sample.cov = cov_mat, sample.nobs = n)
+fitmeasures(fit_cfa, c('cfi', 'tli', 'srmr', 'rmsea', 'chisq', 'df', 'pvalue'))
+summary(fit_cfa) # loadings
+standardizedSolution(fit_cfa) # Standardized solution & Reliability
+fit_cfa.mi <- modificationIndices(fit_cfa) # modification indices (mi)
+fit_cfa.mi[order(fit_cfa.mi$mi, decreasing = TRUE),] # rank relationship by decreasing mi
+
+
+# Multigroup analysis -----------------------------------------------------
+
 
