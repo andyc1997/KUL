26f0ef1958b2036115f3a7852cc9e0b5c4e1f88a
Gml717 <72389432+Gml717@users.noreply.github.com>
2020-11-03 18:13:24 +0100

    Update question4.R

--- question4.R
+++ question4.R
@@ -14,22 +14,31 @@ data_sem <- wvs %>% filter(country %in% c('Netherlands', 'Malaysia')) %>%
 # Structural equation model  ----------------------------------------
 
 # latent variable definitions
-
 sem1 <- '
 
- religion =~ R_attend_religious_services +  R_pray  + R_importance_God
- 
- fraudulent =~ J_claiming_benefits + J_avoiding_fare +
-                J_stealing_property + J_cheating_taxes + J_accept_bribe
-                
- marriage =~ J_homosexuality + J_prostitution + J_abortion + 
-                J_divorce + J_sex_before_marriage + J_suicide
- violent =~ J_beat_wife + J_parents_beating_children + J_violence + J_suicide
- 
+# latent variable definitions
 
- religion =~ fraudulent
- religion =~ marriage
- religion =~ violent'
+religion =~ R_attend_religious_services +  R_pray  + R_importance_God
+
+fraudulent =~ NA*J_claiming_benefits + J_avoiding_fare +
+  J_stealing_property + J_cheating_taxes + J_accept_bribe
+  
+marriage =~ NA*J_homosexuality + J_prostitution + J_abortion + 
+  J_divorce + J_sex_before_marriage + J_suicide
+  
+violent =~ NA*J_beat_wife + J_parents_beating_children + J_violence + J_suicide
+
+fraudulent ~~ 1*fraudulent
+marriage ~~ 1*marriage
+violent ~~ 1*violent
+fraudulent ~~ violent
+marriage ~~ violent
+J_homosexuality ~~ J_sex_before_marriage
+# regressions 
+
+ fraudulent ~ religion
+ marriage ~ religion
+ violent ~ religion'
 
  fitsem1 <- sem(sem1, data = data_sem)
 
